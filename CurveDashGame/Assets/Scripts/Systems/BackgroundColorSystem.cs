2a3dca46918fa023489acb3ba14ffb479e5e76b2
TITAN-DEV <t.p.tai93@gmail.com>
2025-04-01 23:13:42 +0700

    * Update to unity 6. * Rename Materials

--- CurveDashGame/Assets/Scripts/Systems/BackgroundColorSystem.cs
+++ CurveDashGame/Assets/Scripts/Systems/BackgroundColorSystem.cs
@@ -15,15 +15,15 @@ namespace STG.CurveDash
         
         public void Initialize()
         {
-            cam = Camera.main;
-            cam.clearFlags = CameraClearFlags.SolidColor;
+            // cam = Camera.main;
+            // cam.clearFlags = CameraClearFlags.SolidColor;
         }
         
         public void Tick()
         {
-            float d = gameSettings.BackgroundChangeDuration;
-            float t = Mathf.PingPong(Time.time, d) / d;
-            cam.backgroundColor = Color.Lerp(gameSettings.BackgroundColor1, gameSettings.BackgroundColor2, t);
+            // float d = gameSettings.BackgroundChangeDuration;
+            // float t = Mathf.PingPong(Time.time, d) / d;
+            // cam.backgroundColor = Color.Lerp(gameSettings.BackgroundColor1, gameSettings.BackgroundColor2, t);
         }
     }
 }
